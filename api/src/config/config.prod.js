2f7e4af409cbad4221fe5b97cdfb52386651526e
ccnuyan <yan@YandeMacBook-Pro.local>
2016-03-14 22:25:32 +0800

    fix some bugs

--- api/src/config/config.prod.js
+++ api/src/config/config.prod.js
@@ -3,8 +3,7 @@ var maker = require('./maker');
 exports.getConfig = function() {
   var config = maker;
 
-  config.dbconfig.name += 'starc3';
+  config.dbconfig.name = 'starc3';
   config.port = '3000';
-
   return config;
 };
--- api/src/config/config.test.js
+++ api/src/config/config.test.js
@@ -3,8 +3,7 @@ var maker = require('./maker');
 exports.getConfig = function() {
   var config = maker;
 
-  config.dbconfig.name += 'starc3_test';
+  config.dbconfig.name = 'starc3_test';
   config.port = '3000';
-
   return config;
 };
